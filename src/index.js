515688faccc24da030af5aeb6a27362a7c8c8055
MbF-Ravi <sathya@madebyfire.com>
2018-05-23 02:26:34 +0530

    redux curd operation created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Context from './Components/Main';
+import ReaduxFile from './Components/Redux';
 import registerServiceWorker from './registerServiceWorker';
+import postReducer from './reducers/postReducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<Context />, document.getElementById('root'));
+const store = createStore(postReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<ReaduxFile />
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
